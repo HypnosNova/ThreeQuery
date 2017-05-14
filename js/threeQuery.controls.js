d5cf124af4e4ab7727fc8376cc8962dc49d01c0b
HypnosNova <601670314@qq.com>
2017-05-14 21:34:26 +0800

    add
    add

--- js/threeQuery.controls.js
+++ js/threeQuery.controls.js
@@ -66,6 +66,24 @@ $$.Controls = {
 		return controls;
 	},
 	createFirstCharacterControls: function(options, blocker) {
+		if(options){
+			if(options.speed){
+				options.speed.x=options.speed.x!=null?options.speed.x:1;
+				options.speed.z=options.speed.z!=null?options.speed.z:1;
+				options.speed.y=options.speed.y!=null?options.speed.y:200;
+			}else{
+				options.speed=new THREE.Vector3(100,200,100);
+			}
+			options.gravity=options.gravity!=null?options.gravity:10;
+			options.tall=options.tall!=null?options.tall:10;
+			if(options.acceleration){
+				options.acceleration.x=options.acceleration.x!=null?options.acceleration.x:1;
+				options.acceleration.z=options.acceleration.z!=null?options.acceleration.z:1;
+			}else{
+				options.acceleration=new THREE.Vector3(1,0,1);
+			}
+		}
+		
 		var controls = new THREE.PointerLockControls($$.global.camera);
 		$$.global.controls = controls;
 		scene.add(controls.getObject());
@@ -126,9 +144,9 @@ $$.Controls = {
 		controls.moveBackward = false;
 		controls.moveLeft = false;
 		controls.moveRight = false;
-		controls.canJump = false;
+		controls.canJump = true;
 		controls.prevTime = performance.now();
-		controls.velocity = new THREE.Vector3();
+		controls.velocity = new THREE.Vector3(0,0,0);
 		//是否使用键盘控制移动
 		if(options && options.keymove) {
 			document.addEventListener('keydown', function(event) {
@@ -150,7 +168,8 @@ $$.Controls = {
 						$$.global.controls.moveRight = true;
 						break;
 					case 32: // space
-						if($$.global.controls.canJump === true) $$.global.controls.velocity.y += 350;
+						
+						if($$.global.controls.canJump === true){$$.global.controls.velocity.y += options.speed.y};
 						$$.global.controls.canJump = false;
 						break;
 				}
@@ -181,20 +200,19 @@ $$.Controls = {
 			if($$.global.controls.enabled) {
 				$$.global.controls.time = performance.now();
 				var delta = ($$.global.controls.time - $$.global.controls.prevTime) / 1000;
-				$$.global.controls.velocity.x -= $$.global.controls.velocity.x * 2.0 * delta;
-				$$.global.controls.velocity.z -= $$.global.controls.velocity.z * 2.0 * delta;
-				$$.global.controls.velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
-				if($$.global.controls.moveForward) $$.global.controls.velocity.z -= 400.0 * delta;
-				if($$.global.controls.moveBackward) $$.global.controls.velocity.z += 400.0 * delta;
-				if($$.global.controls.moveLeft) $$.global.controls.velocity.x -= 400.0 * delta;
-				if($$.global.controls.moveRight) $$.global.controls.velocity.x += 400.0 * delta;
-
+				$$.global.controls.velocity.x -= $$.global.controls.velocity.x * options.acceleration.x * delta;
+				$$.global.controls.velocity.z -= $$.global.controls.velocity.z * options.acceleration.z * delta;
+				$$.global.controls.velocity.y -= options.gravity * 100.0 * delta; // 100.0 = mass
+				if($$.global.controls.moveForward) $$.global.controls.velocity.z -= options.speed.z * delta;
+				if($$.global.controls.moveBackward) $$.global.controls.velocity.z += options.speed.z * delta;
+				if($$.global.controls.moveLeft) $$.global.controls.velocity.x -= options.speed.x * delta;
+				if($$.global.controls.moveRight) $$.global.controls.velocity.x += options.speed.x * delta;
 				$$.global.controls.getObject().translateX($$.global.controls.velocity.x * delta);
 				$$.global.controls.getObject().translateY($$.global.controls.velocity.y * delta);
 				$$.global.controls.getObject().translateZ($$.global.controls.velocity.z * delta);
-				if($$.global.controls.getObject().position.y < 10) {
+				if($$.global.controls.getObject().position.y < options.tall) {
 					$$.global.controls.velocity.y = 0;
-					$$.global.controls.getObject().position.y = 10;
+					$$.global.controls.getObject().position.y = options.tall;
 					$$.global.controls.canJump = true;
 				}
 				$$.global.controls.prevTime = $$.global.controls.time;
--- js/threeQuery.js
+++ js/threeQuery.js
@@ -1063,6 +1063,24 @@ $$.Controls = {
 		return controls;
 	},
 	createFirstCharacterControls: function(options, blocker) {
+		if(options){
+			if(options.speed){
+				options.speed.x=options.speed.x!=null?options.speed.x:1;
+				options.speed.z=options.speed.z!=null?options.speed.z:1;
+				options.speed.y=options.speed.y!=null?options.speed.y:200;
+			}else{
+				options.speed=new THREE.Vector3(100,200,100);
+			}
+			options.gravity=options.gravity!=null?options.gravity:10;
+			options.tall=options.tall!=null?options.tall:10;
+			if(options.acceleration){
+				options.acceleration.x=options.acceleration.x!=null?options.acceleration.x:1;
+				options.acceleration.z=options.acceleration.z!=null?options.acceleration.z:1;
+			}else{
+				options.acceleration=new THREE.Vector3(1,0,1);
+			}
+		}
+		
 		var controls = new THREE.PointerLockControls($$.global.camera);
 		$$.global.controls = controls;
 		scene.add(controls.getObject());
@@ -1123,9 +1141,9 @@ $$.Controls = {
 		controls.moveBackward = false;
 		controls.moveLeft = false;
 		controls.moveRight = false;
-		controls.canJump = false;
+		controls.canJump = true;
 		controls.prevTime = performance.now();
-		controls.velocity = new THREE.Vector3();
+		controls.velocity = new THREE.Vector3(0,0,0);
 		//是否使用键盘控制移动
 		if(options && options.keymove) {
 			document.addEventListener('keydown', function(event) {
@@ -1147,7 +1165,8 @@ $$.Controls = {
 						$$.global.controls.moveRight = true;
 						break;
 					case 32: // space
-						if($$.global.controls.canJump === true) $$.global.controls.velocity.y += 350;
+						
+						if($$.global.controls.canJump === true){console.log(options.speed.y); $$.global.controls.velocity.y += options.speed.y};
 						$$.global.controls.canJump = false;
 						break;
 				}
@@ -1176,22 +1195,23 @@ $$.Controls = {
 		controls.prevTime = performance.now();
 		controls.update = function() {
 			if($$.global.controls.enabled) {
+				console.log(controls.velocity)
 				$$.global.controls.time = performance.now();
 				var delta = ($$.global.controls.time - $$.global.controls.prevTime) / 1000;
-				$$.global.controls.velocity.x -= $$.global.controls.velocity.x * 2.0 * delta;
-				$$.global.controls.velocity.z -= $$.global.controls.velocity.z * 2.0 * delta;
-				$$.global.controls.velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
-				if($$.global.controls.moveForward) $$.global.controls.velocity.z -= 400.0 * delta;
-				if($$.global.controls.moveBackward) $$.global.controls.velocity.z += 400.0 * delta;
-				if($$.global.controls.moveLeft) $$.global.controls.velocity.x -= 400.0 * delta;
-				if($$.global.controls.moveRight) $$.global.controls.velocity.x += 400.0 * delta;
-
+				$$.global.controls.velocity.x -= $$.global.controls.velocity.x * options.acceleration.x * delta;
+				$$.global.controls.velocity.z -= $$.global.controls.velocity.z * options.acceleration.z * delta;
+				$$.global.controls.velocity.y -= options.gravity * 100.0 * delta; // 100.0 = mass
+				if($$.global.controls.moveForward) $$.global.controls.velocity.z -= options.speed.z * delta;
+				if($$.global.controls.moveBackward) $$.global.controls.velocity.z += options.speed.z * delta;
+				if($$.global.controls.moveLeft) $$.global.controls.velocity.x -= options.speed.x * delta;
+				if($$.global.controls.moveRight) $$.global.controls.velocity.x += options.speed.x * delta;
+				console.log($$.global.controls.velocity.y)
 				$$.global.controls.getObject().translateX($$.global.controls.velocity.x * delta);
 				$$.global.controls.getObject().translateY($$.global.controls.velocity.y * delta);
 				$$.global.controls.getObject().translateZ($$.global.controls.velocity.z * delta);
-				if($$.global.controls.getObject().position.y < 10) {
+				if($$.global.controls.getObject().position.y < options.tall) {
 					$$.global.controls.velocity.y = 0;
-					$$.global.controls.getObject().position.y = 10;
+					$$.global.controls.getObject().position.y = options.tall;
 					$$.global.controls.canJump = true;
 				}
 				$$.global.controls.prevTime = $$.global.controls.time;
