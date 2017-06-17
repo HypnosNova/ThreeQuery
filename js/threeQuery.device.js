3274afe4c68e901875e9f9c97ed38ecb342929c4
HypnosNova <601670314@qq.com>
2017-06-17 12:11:16 +0800

    优化代码

--- js/threeQuery.device.js
+++ js/threeQuery.device.js
@@ -15,5 +15,56 @@ $$.Device = {
 			isAndroid: isAndroid,
 			isPc: isPc
 		};
+	},
+	openWebcam: function(video) {
+		navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+		window.URL = window.URL || window.webkitURL;
+		if(navigator.getUserMedia) {
+			navigator.getUserMedia({
+				video: true
+			}, gotStream, noStream);
+		}
+		var stream;
+		function gotStream(s) {
+			$$.global.webCamSteam=s;
+			stream = s;
+			if(window.URL) {
+				video.src = window.URL.createObjectURL(stream);
+			} else {
+				video.src = stream;
+			}
+			camvideo.onerror = function(e) {
+				stream.stop();
+			};
+			stream.onended = noStream;
+		}
+
+		function noStream(e) {
+			var msg = 'No camera available.';
+			if(e.code == 1) {
+				msg = 'User denied access to use camera.';
+			}
+			console.log(msg)
+		}
+	},
+	closeWebcam:function(){
+		if($$.global.webCamSteam){
+			var tracks=$$.global.webCamSteam.getVideoTracks();
+			for(var i =0;i<tracks.length;i++){
+				tracks[i].stop();
+			}
+			tracks=$$.global.webCamSteam.getAudioTracks();
+			for(var i =0;i<tracks.length;i++){
+				tracks[i].stop();
+			}
+			$$.global.webCamSteam=null;
+		}
+	},
+	toggleWebCam:function(video){
+		if($$.global.webCamSteam){
+			$$.Device.closeWebcam();
+		}else{
+			$$.Device.openWebcam(video);
+		}
 	}
 };
