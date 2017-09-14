ecc6cd57743a16f5d4a36e4e03815f1b9b7b85cb
HypnosNova <601670314@qq.com>
2017-09-14 12:16:50 +0800

    add

--- js/threeQuery.dom.js
+++ js/threeQuery.dom.js
@@ -15,32 +15,38 @@ $$.DOM = function() {
 
 $$.Body = function(css) {
 	$$.DOM.call(this);
-	var that=this;
+	var that = this;
 	this.css = $$.extends({}, [this.css, css]);
 	this.canvas = document.createElement("canvas");
-	this.distanceFromCamera=50;
-	var canvas=this.canvas;
-//	this.text=text;
-	this.update=function(){
-		canvas.width=this.css.width;
-		canvas.height=this.css.height;
+	this.distanceFromCamera = 50;
+	var canvas = this.canvas;
+	//	this.text=text;
+	this.update = function() {
+		canvas.width = this.css.width;
+		canvas.height = this.css.height;
 		let ctx = this.canvas.getContext("2d");
 		ctx.fillStyle = this.css.backgroundColor;
 		ctx.fillRect(0, 0, this.css.width, this.css.height);
-		var spriteMaterial = new THREE.SpriteMaterial( { map: new THREE.CanvasTexture(canvas), color: 0xffffff } );
-		sprite.material=spriteMaterial;
-		sprite.scale.set( this.css.width/4, this.css.height/4, 1 );
+		var spriteMaterial = new THREE.SpriteMaterial({
+			map: new THREE.CanvasTexture(canvas),
+			color: 0xffffff
+		});
+		sprite.material = spriteMaterial;
+		sprite.scale.set(this.css.width / 4, this.css.height / 4, 1);
 	}
-	
-	var spriteMaterial = new THREE.SpriteMaterial( { map: canvas, color: 0xffffff } );
-	var sprite = new THREE.Sprite( spriteMaterial );
+
+	var spriteMaterial = new THREE.SpriteMaterial({
+		map: canvas,
+		color: 0xffffff
+	});
+	var sprite = new THREE.Sprite(spriteMaterial);
 	var vector = new THREE.Vector3(); // create once and reuse it!
-	this.lockToScreen=function(){
-		$$.global.camera.getWorldDirection( vector );
-		that.position.set(vector.x*that.distanceFromCamera,vector.y*that.distanceFromCamera,vector.z*that.distanceFromCamera);
+	this.lockToScreen = function() {
+		$$.global.camera.getWorldDirection(vector);
+		that.position.set(vector.x * that.distanceFromCamera, vector.y * that.distanceFromCamera, vector.z * that.distanceFromCamera);
 		that.lookAt($$.global.camera.position);
 	}
-	
+
 	this.update();
 	this.element = sprite;
 	this.add(this.element);
@@ -53,7 +59,7 @@ $$.Body = function(css) {
 
 $$.Txt = function(text, css) {
 	$$.DOM.call(this);
-	var that=this;
+	var that = this;
 	this.css = $$.extends({}, [this.css, {
 		fontSize: 12,
 		fontWeight: "normal",
@@ -62,26 +68,32 @@ $$.Txt = function(text, css) {
 		textAlign: "center"
 	}, css]);
 	this.canvas = document.createElement("canvas");
-	var canvas=this.canvas;
-	this.text=text;
-	this.update=function(){
-		canvas.width=this.css.width;
-		canvas.height=this.css.height;
+	var canvas = this.canvas;
+	this.text = text;
+	this.update = function() {
+		canvas.width = this.css.width;
+		canvas.height = this.css.height;
 		let ctx = this.canvas.getContext("2d");
 		ctx.fillStyle = this.css.backgroundColor;
 		ctx.fillRect(0, 0, this.css.width, this.css.height);
 		ctx.textAlign = this.css.textAlign;
-		ctx.font = this.css.fontWeight + " " + this.css.fontSize  + "px " + this.css.fontFamily;
+		ctx.font = this.css.fontWeight + " " + this.css.fontSize + "px " + this.css.fontFamily;
 		ctx.fillStyle = this.css.color;
 		let width = ctx.measureText(text).width;
-		ctx.fillText(text, this.css.width / 2, this.css.height/2+this.css.fontSize/4);
-		var spriteMaterial = new THREE.SpriteMaterial( { map: new THREE.CanvasTexture(canvas), color: 0xffffff } );
-		sprite.material=spriteMaterial;
-		sprite.scale.set( this.css.width/4, this.css.height/4, 1 );
+		ctx.fillText(text, this.css.width / 2, this.css.height / 2 + this.css.fontSize / 4);
+		var spriteMaterial = new THREE.SpriteMaterial({
+			map: new THREE.CanvasTexture(canvas),
+			color: 0xffffff
+		});
+		sprite.material = spriteMaterial;
+		sprite.scale.set(this.css.width / 4, this.css.height / 4, 1);
 	}
-	
-	var spriteMaterial = new THREE.SpriteMaterial( { map: canvas, color: 0xffffff } );
-	var sprite = new THREE.Sprite( spriteMaterial );
+
+	var spriteMaterial = new THREE.SpriteMaterial({
+		map: canvas,
+		color: 0xffffff
+	});
+	var sprite = new THREE.Sprite(spriteMaterial);
 	this.update();
 	this.element = sprite;
 	this.add(this.element);
@@ -92,31 +104,37 @@ $$.Txt = function(text, css) {
 	$$.Txt.prototype = new Super();
 })();
 
-$$.Img = function(url,css){
+$$.Img = function(url, css) {
 	$$.DOM.call(this);
-	var that=this;
-	
-	if($$.Loader.RESOURCE.textures[url]){
-		var spriteMaterial = new THREE.SpriteMaterial( { map: $$.Loader.RESOURCE.textures[url], color: 0xffffff } );
-		var sprite = new THREE.Sprite( spriteMaterial );
+	var that = this;
+
+	if($$.Loader.RESOURCE.textures[url]) {
+		var spriteMaterial = new THREE.SpriteMaterial({
+			map: $$.Loader.RESOURCE.textures[url],
+			color: 0xffffff
+		});
+		var sprite = new THREE.Sprite(spriteMaterial);
 		that.element = sprite;
 		that.add(that.element);
 		this.css = $$.extends({}, [this.css, {
-			width:$$.Loader.RESOURCE.textures[url].image.naturalWidth,
-			height:$$.Loader.RESOURCE.textures[url].image.naturalHeight,
+			width: $$.Loader.RESOURCE.textures[url].image.naturalWidth,
+			height: $$.Loader.RESOURCE.textures[url].image.naturalHeight,
 		}, css]);
-		sprite.scale.set( this.css.width/4, this.css.height/4, 1 );
-	}else{
-		$$.Loader.loadTexture([url],function(texture){
-			var spriteMaterial = new THREE.SpriteMaterial( { map: texture, color: 0xffffff } );
-			var sprite = new THREE.Sprite( spriteMaterial );
+		sprite.scale.set(this.css.width / 4, this.css.height / 4, 1);
+	} else {
+		$$.Loader.loadTexture([url], function(texture) {
+			var spriteMaterial = new THREE.SpriteMaterial({
+				map: texture,
+				color: 0xffffff
+			});
+			var sprite = new THREE.Sprite(spriteMaterial);
 			that.element = sprite;
 			that.add(that.element);
 			this.css = $$.extends({}, [this.css, {
-				width:texture.image.naturalWidth,
-				height:texture.image.naturalHeight,
+				width: texture.image.naturalWidth,
+				height: texture.image.naturalHeight,
 			}, css]);
-			sprite.scale.set( this.css.width/4, this.css.height/4, 1 );
+			sprite.scale.set(this.css.width / 4, this.css.height / 4, 1);
 		});
 	}
 };
@@ -124,4 +142,30 @@ $$.Img = function(url,css){
 	var Super = function() {};
 	Super.prototype = $$.DOM.prototype;
 	$$.Img.prototype = new Super();
+})();
+
+$$.Video = function(url, css) {
+	$$.DOM.call(this);
+	var that = this;
+	this.video=document.createElement("video");
+	this.video.src=url;
+	var texture=new THREE.VideoTexture(this.video);
+	var spriteMaterial = new THREE.SpriteMaterial({
+		map: texture,
+		color: 0xffffff
+	});
+	var sprite = new THREE.Sprite(spriteMaterial);
+	that.element = sprite;
+	that.add(that.element);
+	console.log(texture)
+	this.css = $$.extends({}, [this.css, {
+		width: texture.image.naturalWidth,
+		height: texture.image.naturalHeight,
+	}, css]);
+	sprite.scale.set(this.css.width / 4, this.css.height / 4, 1);
+};
+(function() {
+	var Super = function() {};
+	Super.prototype = $$.DOM.prototype;
+	$$.Video.prototype = new Super();
 })();
